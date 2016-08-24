cd553e1179c7ff6f02a0b317fe22297bad14b256
Chris <chriscrawf95@gmail.com>
2016-08-23 22:12:35 -0700

    Settings: Add CypherOS version to about phone

--- src/com/android/settings/DeviceInfoSettings.java
+++ src/com/android/settings/DeviceInfoSettings.java
@@ -82,6 +82,7 @@ public class DeviceInfoSettings extends SettingsPreferenceFragment implements In
     private static final String PROPERTY_EQUIPMENT_ID = "ro.ril.fccid";
     private static final String KEY_DEVICE_FEEDBACK = "device_feedback";
     private static final String KEY_SAFETY_LEGAL = "safetylegal";
+	private static final String KEY_CYPHER_VERSION = "cypher_version";
 
     static final int TAPS_TO_BE_A_DEVELOPER = 7;
 
@@ -129,6 +130,7 @@ public class DeviceInfoSettings extends SettingsPreferenceFragment implements In
         setStringSummary(KEY_BUILD_NUMBER, Build.DISPLAY);
         findPreference(KEY_BUILD_NUMBER).setEnabled(true);
         findPreference(KEY_KERNEL_VERSION).setSummary(getFormattedKernelVersion());
+		setValueSummary(KEY_PA_VERSION, "ro.cypher.version");
 
         if (!SELinux.isSELinuxEnabled()) {
             String status = getResources().getString(R.string.selinux_status_disabled);
